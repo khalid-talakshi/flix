aa6e13f04f52a0b2bb3e189c3f6040c69faf8ab4
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-06-25 14:15:38 -0400

    rubocop

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,12 +4,15 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
   def show
     @movie = Movie.find(params[:id])
   end
+
   def edit
     @movie = Movie.find(params[:id])
   end
+
   def update
     @movie = Movie.find(params[:id])
     movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "movies#index"
+  root 'movies#index'
   get 'movies' => 'movies#index'
-  get '/movies/:id' => 'movies#show', as: "movie"
-  get '/movies/:id/edit' => 'movies#edit', as: "edit_movie"
-  patch '/movies/:id' => "movies#update"
+  get '/movies/:id' => 'movies#show', as: 'movie'
+  get '/movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+  patch '/movies/:id' => 'movies#update'
 end
