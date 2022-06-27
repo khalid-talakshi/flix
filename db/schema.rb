a6551c06dd37e932a725afce34d5214e0a971d56
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-06-27 14:25:58 -0400

    Added duration, director and image to movies

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_625_162_028) do
-  create_table 'movies', force: :cascade do |t|
-    t.string 'title'
-    t.string 'rating'
-    t.decimal 'total_gross'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.text 'description'
-    t.date 'released_on'
+ActiveRecord::Schema.define(version: 2022_06_27_181547) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.decimal "total_gross"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: "placeholder.png"
   end
+
 end
